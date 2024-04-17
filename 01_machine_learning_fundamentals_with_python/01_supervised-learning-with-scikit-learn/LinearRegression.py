9d474bc2305a984cafcffe6dfddc04675009036a
Zoltan Tarko <zoltantarko@gmail.com>
2024-04-17 23:25:48 +0300

    add section

---
+++ 01_machine_learning_fundamentals_with_python/01_supervised-learning-with-scikit-learn/LinearRegression.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+sales_df = pd.read_csv('../advertising_and_sales_clean.csv', header=0)
+
+print(sales_df)
+# Create X from the radio column's values
+X = sales_df["radio"].values
+
+# Create y from the sales column's values
+y = sales_df["sales"].values
+
+# Reshape X
+X = X.reshape(-1, 1)
+
+# Check the shape of the features and targets
+print(X.shape, y.shape)
+# Create the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X, y)
+
+# Make predictions
+predictions = reg.predict(X)
+
+print(predictions[:5])
+# Create scatter plot
+plt.scatter(X, y, color="blue")
+
+# Create line plot
+plt.plot(X, predictions, color="red")
+plt.xlabel("Radio Expenditure ($)")
+plt.ylabel("Sales ($)")
+
+# Display the plot
+plt.show()
