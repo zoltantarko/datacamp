86eb912370a6387bca030ac9e3b9445fa816f242
Zoltan Tarko <zoltantarko@gmail.com>
2024-04-17 22:55:05 +0300

    KnnFit

---
+++ 01_machine_learning_fundamentals_with_python/01_supervised-learning-with-scikit-learn/KnnFit.py
@@ -0,0 +1,17 @@
+# Import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+import pandas as pd
+
+X_new = pd.DataFrame({'account_length': [30.0, 107.0, 213.0], 'customer_service_calls': [17.5, 24.1, 10.9]})
+
+churn_df = pd.read_csv('../churn_df.csv', header=0)
+y = churn_df["churn"].values
+X = churn_df[["account_length", "customer_service_calls"]].values
+
+# Create a KNN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+# Fit the classifier to the data
+knn.fit(X, y)
+#print(knn.score)
+y_pred = knn.predict(X_new)
+print("Predictions: {}".format(y_pred)) 
