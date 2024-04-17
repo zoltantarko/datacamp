cfb29eaf9505eb1d6a17b2d7c134f559f87487e2
Zoltan Tarko <zoltantarko@gmail.com>
2024-04-17 23:05:49 +0300

    Added sections

---
+++ 01_machine_learning_fundamentals_with_python/01_supervised-learning-with-scikit-learn/KnnOverUnderFitting.py
@@ -0,0 +1,45 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+churn_df = pd.read_csv('../churn_df.csv', header=0)
+X = churn_df.drop("churn", axis=1).values
+y = churn_df["churn"].values
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# Create neighbors
+neighbors = np.arange(1, 13)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+  
+	# Set up a KNN Classifier
+	knn = KNeighborsClassifier(n_neighbors=neighbor)
+  
+	# Fit the model
+	knn.fit(X_train, y_train)
+  
+	# Compute accuracy
+	train_accuracies[neighbor] = knn.score(X_train, y_train)
+	test_accuracies[neighbor] = knn.score(X_test, y_test)
+print(neighbors, '\n', train_accuracies, '\n', test_accuracies)
+# Add a title
+plt.title("KNN: Varying Number of Neighbors")
+
+# Plot training accuracies
+plt.plot(neighbors, train_accuracies.values(), label="Training Accuracy")
+
+# Plot test accuracies
+plt.plot(neighbors, test_accuracies.values(), label="Testing Accuracy")
+
+plt.legend()
+plt.xlabel("Number of Neighbors")
+plt.ylabel("Accuracy")
+
+# Display the plot
+plt.show()
