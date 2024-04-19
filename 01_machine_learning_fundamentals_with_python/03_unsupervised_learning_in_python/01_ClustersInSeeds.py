0458dba95751891a00f28f5cda1a97ff20065e1c
Zoltan Tarko <zoltantarko@gmail.com>
2024-04-19 09:49:48 +0300

    add sections

---
+++ 01_machine_learning_fundamentals_with_python/03_unsupervised_learning_in_python/01_ClustersInSeeds.py
@@ -0,0 +1,27 @@
+# Import KMeans
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+
+grains = np.loadtxt("../00_resources/seeds_dataset.txt")
+
+ks = range(1, 6)
+inertias = []
+
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)
+    
+    # Fit model to samples
+    model.fit(grains)
+    
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+    
+# Plot ks vs inertias
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
+
